2638db0fdbb3c6081dd4e3dcafe1a7a0f86f9bcf
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-11-23 09:15:55 +0100

    feat: set mock empty item properties to navbar

--- app/View/Components/Navbar.php
+++ app/View/Components/Navbar.php
@@ -8,12 +8,23 @@ use Illuminate\View\Component;
 
 class Navbar extends Component
 {
+    public array $menu_items;
+
+    public array $home_menu_items;
+
+    public array $admin_menu_items;
+
     /**
      * Create a new component instance.
      */
     public function __construct()
     {
         //
+        $this->menu_items = [];
+
+        $this->home_menu_items = [];
+
+        $this->admin_menu_items = [];
     }
 
     /**
