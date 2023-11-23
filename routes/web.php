ebdd3d8a9a244eb01731fdbe0c772444e26771cc
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-11-23 10:48:07 +0100

    feat: add playlists routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+Route::get('playlists', [\App\Http\Controllers\PlaylistController::class, 'index'])->name('playlists.index');
+Route::get('playlists/{playlist:slug}', [\App\Http\Controllers\PlaylistController::class, 'show'])->name('playlists.show');
 
 Route::view('/dashboard', 'dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
