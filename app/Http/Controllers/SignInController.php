d95d1f30e556322b471423fd7f374fb9c9d5590d
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-11-23 02:16:36 +0100

    feat(SignInController): add

---
+++ app/Http/Controllers/SignInController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SignInRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
+
+class SignInController extends Controller
+{
+    public function index(): View
+    {
+        return view('auth.signin');
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function store(SignInRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return redirect()->intended(RouteServiceProvider::HOME);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
