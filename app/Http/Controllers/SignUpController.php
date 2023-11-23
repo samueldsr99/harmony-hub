8b074d528de48dbfe3e524b7da1c06999a8ac96c
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-11-23 02:16:43 +0100

    feat(SignUpController): add

---
+++ app/Http/Controllers/SignUpController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Validation\ValidationException;
+
+class SignUpController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('auth.signup');
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+//        $user->notify(new WelcomeNewUser());
+
+        $admin = User::where('is_admin', true)->first();
+//        $admin?->notify(new InformAdminOfNewUser($user));
+
+        Auth::login($user);
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
