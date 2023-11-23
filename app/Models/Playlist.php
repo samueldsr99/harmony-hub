aeb3c33d85e35a6fd6019ef236b8247fa794e23d
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-11-23 21:49:49 +0100

    feat: add media support for Playlist

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -8,12 +8,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-final class Playlist extends Model
+final class Playlist extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $fillable = ['title', 'author_id'];
+
+    // Model events -----------------------------------------------------------
+    protected static function booted()
+    {
+        static::creating(function ($playlist) {
+            $playlist->slug = $playlist->slug ?? $playlist->createUniqueSlug();
+        });
+    }
 
     // Model Relationships -----------------------------------------------------
     public function author(): BelongsTo
@@ -29,5 +43,46 @@ final class Playlist extends Model
             'playlist_id',
             'song_id'
         )->withTimestamps();
+    }
+
+    // Model attributes --------------------------------------------------------
+    public function getThumbnailUrlAttribute(): string
+    {
+        return $this->getImageUrl('thumbnail');
+    }
+
+    // Model method ------------------------------------------------------------------
+    public function getImageUrl(string $conversion): string
+    {
+        return ($this->media->isNotEmpty())
+            ? $this->media->first()->getUrl($conversion)
+            : '/media/default/conversions/default-'.$conversion.'.jpg';
+    }
+
+    // Medialibrary settings ----------------------------------------------------------
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->fit(Manipulations::FIT_CROP, 300, 200)
+            ->nonQueued();
+        $this
+            ->addMediaConversion('thumbnail')
+            ->fit(Manipulations::FIT_CROP, 50, 50)
+            ->nonQueued();
+
+    }
+
+    public function createUniqueSlug(): string
+    {
+        $nr = 0;
+        do {
+            $slug = ($nr === 0)
+                ? Str::slug($this->title)
+                : Str::slug($this->title).'-'.$nr;
+            $nr++;
+        } while (Playlist::where('slug', $slug)->where('id', '<>', $this->id)->count() > 0);
+
+        return $slug;
     }
 }
--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,15 @@ return [
             'throw' => false,
         ],
 
+        'media' => [
+            'driver' => 'local',
+            'root' => storage_path('app/media'),
+            'url' => env('APP_URL').'/media',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
@@ -71,6 +80,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('media') => storage_path('app/media'),
     ],
 
 ];
