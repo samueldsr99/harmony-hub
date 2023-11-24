98fd503a807e2c4677088cfd65017830ea67f3c9
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-11-24 02:45:55 +0100

    bugfix(navbar): broken links

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -123,15 +123,6 @@
 
         </div>
         <div class="border-t border-gray-700 pb-3 pt-4">
-            <div class="flex items-center px-5">
-                <div class="flex-shrink-0">
-                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                </div>
-                <div class="ml-3">
-                    <div class="text-base font-medium leading-none text-white">Tom Cook</div>
-                    <div class="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
-                </div>
-            </div>
             <div class="mt-3 space-y-1 px-2">
                 @foreach($admin_menu_items as $item)
                     @if(request()->routeIs($item['route']))
@@ -143,9 +134,11 @@
 
 
 
-                <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your Profile</a>
-                <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
-                <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign out</a>
+                <a href="{{route('profile.edit')}}" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your Profile</a>
+                <form method="POST" action="{{ route('signout') }}">
+                    @csrf
+                    <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white" role="menuitem" tabindex="-1" id="user-menu-item-2" onclick="event.preventDefault();this.closest('form').submit();">Sign out</a>
+                </form>
             </div>
         </div>
     </div>
