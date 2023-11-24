912123566d48a1c1904ff39ef8fd0db641d689b5
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-11-24 02:39:36 +0100

    bugfix: remove dark bg

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,7 @@
 <body class="font-sans text-gray-900 antialiased">
 <x-navbar />
 
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     {{ $slot }}
 </div>
 </body>
