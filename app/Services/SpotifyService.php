bd6fa6b6489af19dbf76884cadf411f22c0954ff
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-01-25 00:18:53 +0100

    feat: cache spotify token & responses

--- app/Services/SpotifyService.php
+++ app/Services/SpotifyService.php
@@ -21,17 +21,35 @@ class SpotifyService
         $this->cache_ttl = config('app.env') === 'production' ? 3600 : 1;
     }
 
-    public function getFeaturedPlaylists()
+    public function getPlaylist(string $spotify_playlist_id)
+    {
+        return cache()->remember(
+            key: 'spotify_playlist_' . $spotify_playlist_id,
+            ttl: $this->cache_ttl,
+            callback: fn() => $this->fetchPlaylist($spotify_playlist_id)
+        );
+    }
+
+    public function fetchPlaylist(string $spotify_playlist_id)
     {
         $response = Http::withHeaders([
             'Authorization' => 'Bearer ' . $this->getToken(),
-        ])->get($this->base_url . '/browse/featured-playlists');
+        ])
+            ->withResponseMiddleware(
+                fn($request, $response) => $this->invalidateWhenUnauthorized($request, $response)
+            )
+            ->get($this->base_url . '/playlists/' . $spotify_playlist_id);
 
         return $response->json();
     }
 
     public function getToken()
     {
+        $access_token = cache('spotify_access_token');
+        if ($access_token) {
+            return $access_token;
+        }
+
         $base64_credentials = base64_encode($this->client_id . ':' . $this->client_secret);
 
         $response = Http::withHeaders([
@@ -43,14 +61,37 @@ class SpotifyService
             'client_secret' => $this->client_secret,
         ]);
 
-        return $response->json()['access_token'];
+        $access_token = $response->json()['access_token'];
+
+        cache(['spotify_access_token' => $access_token], $this->cache_ttl);
+        return $access_token;
     }
 
-    public function getPlaylist(string $spotify_playlist_id)
+    private function invalidateWhenUnauthorized($request, $response): Http
     {
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . $this->getToken(),
-        ])->get($this->base_url . '/playlists/' . $spotify_playlist_id);
+        if ($response->status() === 401) {
+            cache()->forget('spotify_access_token');
+        }
+        return $response;
+    }
+
+    public function getFeaturedPlaylists()
+    {
+        return cache()->remember(
+            key: 'spotify_featured_playlists',
+            ttl: $this->cache_ttl,
+            callback: fn() => $this->fetchFeaturedPlaylists()
+        );
+    }
+
+    private function fetchFeaturedPlaylists()
+    {
+        $response = Http
+            ::withHeaders(['Authorization' => 'Bearer ' . $this->getToken()])
+            ->withResponseMiddleware(
+                fn($request, $response) => $this->invalidateWhenUnauthorized($request, $response)
+            )
+            ->get($this->base_url . '/browse/featured-playlists');
 
         return $response->json();
     }
