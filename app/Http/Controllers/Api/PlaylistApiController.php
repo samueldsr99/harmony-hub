a2592cd6b5280d9d4b83e301fbc7b51dfd90351d
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-01-25 01:10:51 +0100

    feat: add `Playlist` api

---
+++ app/Http/Controllers/Api/PlaylistApiController.php
@@ -0,0 +1,68 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PlaylistRequest;
+use App\Http\Requests\PlaylistUpdateRequest;
+use App\Http\Resources\PlaylistResource;
+use App\Models\Playlist;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class PlaylistApiController extends Controller
+{
+    public function index()
+    {
+        return PlaylistResource::collection(Playlist::with('songs')->paginate(25));
+    }
+
+    public function show(int $playlist_id)
+    {
+        try {
+            $playlist = Playlist::with('songs')->findOrFail($playlist_id);
+            return new PlaylistResource($playlist);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Playlist not found',
+            ], 404);
+        }
+    }
+
+    public function store(PlaylistRequest $request)
+    {
+        try {
+            $playlist = Playlist::create($request->all());
+            return new PlaylistResource($playlist);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Playlist not found',
+            ], 404);
+        }
+    }
+
+    public function update(PlaylistUpdateRequest $request)
+    {
+        try {
+            $playlist = Playlist::findOrFail($request->playlist_id);
+            $playlist->update($request->all());
+            return new PlaylistResource($playlist);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Playlist not found',
+            ], 404);
+        }
+    }
+
+    public function destroy(int $playlist_id)
+    {
+        try {
+            $playlist = Playlist::findOrFail($playlist_id);
+            $playlist->delete();
+            return response()->json([
+                'message' => 'Playlist deleted',
+            ], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Playlist not found',
+            ], 404);
+        }
+    }
+}
---
+++ app/Http/Requests/PlaylistRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PlaylistRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'min:5', 'max:20'],
+            'songs' => ['required', 'array'],
+            'songs.*.title' => ['required', 'string'],
+            'songs.*.author' => ['required', 'string'],
+            'songs.*.genre' => ['required', 'string'],
+            'image' => ['file'],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Validation failed',
+            'errors' => $validator->errors(),
+        ], 422);
+    }
+}
