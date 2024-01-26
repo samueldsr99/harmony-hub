632696a6583150b689b1996ce6dd6470a90948fa
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-01-26 22:39:54 +0100

    bugfix: authorize `PlaylistStoreRequest`, `PlaylistUpdateRequest` requests

--- app/Http/Requests/PlaylistStoreRequest.php
+++ app/Http/Requests/PlaylistStoreRequest.php
@@ -12,7 +12,7 @@ class PlaylistStoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/PlaylistUpdateRequest.php
+++ app/Http/Requests/PlaylistUpdateRequest.php
@@ -12,7 +12,7 @@ class PlaylistUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
